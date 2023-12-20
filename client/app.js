48ee10aa2671bf0d4667431a4ed44bee75b58868
Junglepanda69 <Conrady82@gmail.com>
2023-12-20 14:27:20 -0800

    blazepose implementation

--- client/app.js
+++ client/app.js
@@ -1,48 +1,56 @@
-document.getElementById("file-upload").addEventListener("change", (event) => {
-  const file = event.target.files[0];
-  const video = document.getElementById("climbing-video");
-  const canvas = document.getElementById("output-canvas");
-  const ctx = canvas.getContext("2d");
+let detector;
 
-  // Load the video file
-  const url = URL.createObjectURL(file);
-  video.src = url;
-  video.hidden = false;
+async function loadBlazePoseModel() {
+  const model = poseDetection.SupportedModels.BlazePose;
+  const detectorConfig = {
+    runtime: "tfjs",
+    enableSmoothing: true,
+    modelType: "full", // You can change to 'lite' or 'heavy'
+  };
+  detector = await poseDetection.createDetector(model, detectorConfig);
+}
 
-  // Load Pose Estimation Model (PoseNet as an example)
-  let net;
-  posenet.load().then((model) => {
-    net = model;
+document
+  .getElementById("file-upload")
+  .addEventListener("change", async (event) => {
+    await loadBlazePoseModel();
+
+    const file = event.target.files[0];
+    const video = document.getElementById("climbing-video");
+    const canvas = document.getElementById("output-canvas");
+    const ctx = canvas.getContext("2d");
+    const url = URL.createObjectURL(file);
+    video.src = url;
+    video.hidden = false;
+
+    video.addEventListener("play", async () => {
+      const intervalId = setInterval(async () => {
+        if (!detector) {
+          console.error("BlazePose model not loaded");
+          return;
+        }
+
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const estimationConfig = { flipHorizontal: false };
+        const poses = await detector.estimatePoses(video, estimationConfig);
+
+        if (poses && poses.length > 0) {
+          drawPose(ctx, poses[0]);
+        }
+      }, 100);
+
+      video.addEventListener("pause", () => clearInterval(intervalId));
+      video.addEventListener("ended", () => clearInterval(intervalId));
+    });
   });
 
-  // Function to process video frame and perform pose estimation
-  function processVideoFrame() {
-    // Ensure the model is loaded
-    if (net) {
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-      // Perform pose estimation
-      net.estimateSinglePose(video).then((pose) => {
-        // Draw detected pose
-        drawPose(pose);
-      });
+function drawPose(ctx, pose) {
+  for (const keypoint of pose.keypoints) {
+    if (keypoint.score > 0.5) {
+      ctx.beginPath();
+      ctx.arc(keypoint.x, keypoint.y, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
     }
   }
-
-  // Draw Pose on Canvas
-  function drawPose(pose) {
-    // Draw each keypoint (e.g., hands, feet)
-    for (let keypoint of pose.keypoints) {
-      if (keypoint.score > 0.5) {
-        ctx.beginPath();
-        ctx.arc(keypoint.position.x, keypoint.position.y, 5, 0, 2 * Math.PI);
-        ctx.fillStyle = "red";
-        ctx.fill();
-      }
-    }
-  }
-
-  // Process each frame when video is playing
-  video.addEventListener("play", () => {
-    setInterval(processVideoFrame, 100); // Adjust interval as needed
-  });
-});
+}
